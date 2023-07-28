1433472d8c45d57e7a54b3031d6fbed193ed29a2
Michael Schimpelsberger <schimi.dev@gmail.com>
2023-07-28 13:29:04 +0200

    simplified server action code

--- src/server/actions.ts
+++ src/server/actions.ts
@@ -4,11 +4,9 @@ import { revalidatePath } from "next/cache";
 import { updateProject } from "./lib";
 
 export const updateProjectAction = async (formData: FormData) => {
-    const data = {
-        id: formData.get("id") as string,
-        name: formData.get("name") as string,
-        description: formData.get("description") as string
-    }
-    await updateProject(data.id, data.name, data.description);
+    const id = formData.get("id") as string;
+    const name = formData.get("name") as string;
+    const description = formData.get("description") as string;
+    await updateProject(id, name, description);
     revalidatePath("/");
 }
